43a251aa9154f2f11287d57e6b72dfc76ec8272a
Tomás Catena <tomascatena@gmail.com>
2021-02-12 19:12:55 +0100

    Initial commit

---
+++ src/components/AddExpensePage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AddExpensePage = () => {
+  return <div>This is from my create expense component</div>;
+};
+
+export default AddExpensePage;
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Expensify</h1>
+      <NavLink activeClassName="is-active" exact={true} to="/">
+        Dashboard
+      </NavLink>
+      <NavLink activeClassName="is-active" to="/create">
+        Create Expense
+      </NavLink>
+      <NavLink activeClassName="is-active" to="/help">
+        Help
+      </NavLink>
+    </header>
+  );
+};
+
+export default Header;
